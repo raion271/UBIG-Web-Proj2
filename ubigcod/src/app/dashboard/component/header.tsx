a7f03f51866413c1e0475be186a60b872a97e8fb
I/M Here <fanzfenze@gmail.com>
2025-07-01 15:39:24 +0700

    nambah theme dugem

--- ubigcod/src/app/dashboard/component/header.tsx
+++ ubigcod/src/app/dashboard/component/header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -5,13 +7,29 @@ import {
     BreadcrumbList,
     BreadcrumbPage,
     BreadcrumbSeparator,
-    BreadcrumbEllipsis,
 } from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import {
     SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 export default function DashboardHeader() {
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+    const toggleTheme = () => {
+        theme === "dark" ? setTheme("light") : setTheme("dark")
+    }
+
+    if (!mounted) {
+        return null
+    }
     return (
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
@@ -33,6 +51,12 @@ export default function DashboardHeader() {
                         </BreadcrumbItem>
                     </BreadcrumbList>
                 </Breadcrumb>
+            </div>
+            <div className="flex items-center gap-2 ml-auto mr-11">
+                <Button variant="outline" size="icon" onClick={toggleTheme} className="relative">
+                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 duration-300" />
+                </Button>
             </div>
         </header>
     )
