08df05370ed8e1591636134e435e7396791ed906
Hafza <muhammadhafzaaldian@gmail.com>
2025-07-01 13:28:24 +0700

    nambahin chart

--- ubigcod/src/app/dashboard/component/main.tsx
+++ ubigcod/src/app/dashboard/component/main.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Card,
     CardContent,
@@ -7,8 +8,40 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { ChartColumn, CircleDollarSign, CreditCard, Users } from "lucide-react"
+import { 
+    Bar, 
+    BarChart,
+    CartesianGrid,
+    XAxis,
+    YAxis
+} from "recharts"
+import { 
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent
+} from "@/components/ui/chart"
 
 export default function DashboardMain() {
+    const chartData = [
+        { month: "January", desktop: 186, mobile: 80 },
+        { month: "February", desktop: 305, mobile: 200 },
+        { month: "March", desktop: 237, mobile: 120 },
+        { month: "April", desktop: 73, mobile: 190 },
+        { month: "May", desktop: 209, mobile: 130 },
+        { month: "June", desktop: 214, mobile: 140 },
+    ]
+    const chartConfig = {
+        desktop: {
+            label: "Desktop",
+            color: "var(--chart-2)",
+        },
+        mobile: {
+            label: "Mobile",
+            color: "var(--chart-3)",
+        },
+    } satisfies ChartConfig
+
     return (
         <>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0 m-5">
@@ -17,63 +50,64 @@ export default function DashboardMain() {
                     <Card>
                         <CardHeader>
                             <CardTitle className="flex justify-between">
-                                <div>Total Revenue</div>
+                                <div className="font-bold">Total Revenue</div>
                                 <CircleDollarSign />
                             </CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <div>$45,431.85</div>
+                            <div className="font-bold text-2xl mb-[-10px]">$45,431.85</div>
                         </CardContent>
                         <CardFooter>
-                            <div>Card Footer</div>
+                            <div className="text-s mt-[-5px]">Card Footer</div>
                         </CardFooter>
                     </Card>
                     {/* subscridivtion */}
                     <Card>
                         <CardHeader>
                             <CardTitle className="flex justify-between">
-                                <div>Subscridivtion</div>
+                                <div className="font-bold">Subscription</div>
                                 <Users />
                             </CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <div>+2,350</div>
+                            <div className="font-bold text-2xl mb-[-10px]">+2,350</div>
                         </CardContent>
                         <CardFooter>
-                            <div>Card Footer</div>
+                            <div className="text-s mt-[-5px]">Card Footer</div>
                         </CardFooter>
                     </Card>
                     {/* salary */}
                     <Card>
                         <CardHeader>
                             <CardTitle className="flex justify-between">
-                                <div>Salary</div>
+                                <div className="font-bold">Salary</div>
                                 <CreditCard />
                             </CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <div>+12,234</div>
+                            <div className="font-bold text-2xl mb-[-10px]">+12,234</div>
                         </CardContent>
                         <CardFooter>
-                            <div>Card Footer</div>
+                            <div className="text-s mt-[-5px]">Card Footer</div>
                         </CardFooter>
                     </Card>
                     {/* active user */}
                     <Card>
                         <CardHeader>
                             <CardTitle className="flex justify-between">
-                                <div>Active User</div>
+                                <div className="font-bold">Active Now</div>
                                 <ChartColumn />
                             </CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <div>Card Content</div>
+                            <div className="font-bold text-2xl mb-[-10px]">+573</div>
                         </CardContent>
                         <CardFooter>
-                            <div>Card Footer</div>
+                            <div className="text-s mt-[-5px]">Card Footer</div>
                         </CardFooter>
                     </Card>
                 </div>
+                {/* baris kedua */}
                 <div className="grid auto-rows-min gap-4 md:grid-cols-2 flex-wrap">
                     <Card>
                         <CardHeader>
@@ -81,23 +115,38 @@ export default function DashboardMain() {
                             <CardDescription>Card Description</CardDescription>
                         </CardHeader>
                         <CardContent>
-                            <p>Card Content</p>
+                            <ChartContainer config={chartConfig} className="h-[200px] w-full">
+                                <BarChart accessibilityLayer data={chartData}>
+                                    <CartesianGrid vertical={true} />
+                                    <XAxis
+                                        dataKey="month"
+                                        tickLine={true}
+                                        tickMargin={10}
+                                        axisLine={true}
+                                        tickFormatter={(value) => value.slice(0, 3)}
+                                    />
+                                    <YAxis
+                                        tickLine={true}
+                                        axisLine={true}
+                                    />
+                                    <ChartTooltip content={<ChartTooltipContent />} />
+                                    <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                                    <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+                                </BarChart>
+                            </ChartContainer>
                         </CardContent>
                         <CardFooter>
-                            <p>Card Footer</p>
+                            <div>Card Footer</div>
                         </CardFooter>
                     </Card>
                     <Card>
                         <CardHeader>
-                            <CardTitle>Card Title</CardTitle>
+                            <CardTitle>Recent Sales</CardTitle>
                             <CardDescription>Card Description</CardDescription>
                         </CardHeader>
                         <CardContent>
-                            <p>Card Content</p>
+                            <div>Card Content</div>
                         </CardContent>
-                        <CardFooter>
-                            <p>Card Footer</p>
-                        </CardFooter>
                     </Card>
                 </div>
             </div>
