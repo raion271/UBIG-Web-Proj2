e629f695ffd56ba45d0572f15ce7d52113ac02c8
Hafza <muhammadhafzaaldian@gmail.com>
2025-07-01 10:35:03 +0700

    nambahin main

--- ubigcod/src/app/dashboard/page.tsx
+++ ubigcod/src/app/dashboard/page.tsx
@@ -1,55 +1,121 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
 } from "@/components/ui/sidebar"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { CircleDollarSign } from 'lucide-react';
 
 export default function Page() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  )
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 data-[orientation=vertical]:h-4"
+                        />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem className="hidden md:block">
+                                    <BreadcrumbLink href="#">
+                                        Building Your Application
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator className="hidden md:block" />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4 pt-0 m-5">
+                    <div className="grid auto-rows-min gap-4 md:grid-cols-4 flex-wrap">
+                        {/* total revenue */}
+                        <Card>
+                            <CardHeader>
+                                <CardTitle className="flex justify-between">
+                                    <div>Card Title</div>
+                                    <CircleDollarSign />
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>Card Content</p>
+                            </CardContent>
+                            <CardFooter>
+                                <p>Card Footer</p>
+                            </CardFooter>
+                        </Card>
+                        {/* subscription */}
+                        <Card>
+                            <CardHeader>
+                                <CardTitle className="flex justify-between">
+                                    <div>Card Title</div>
+                                    <CircleDollarSign />
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>Card Content</p>
+                            </CardContent>
+                            <CardFooter>
+                                <p>Card Footer</p>
+                            </CardFooter>
+                        </Card>
+                        {/* salary */}
+                        <Card>
+                            <CardHeader>
+                                <CardTitle className="flex justify-between">
+                                    <div>Card Title</div>
+                                    <CircleDollarSign />
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>Card Content</p>
+                            </CardContent>
+                            <CardFooter>
+                                <p>Card Footer</p>
+                            </CardFooter>
+                        </Card>
+                        {/* active user */}
+                        <Card>
+                            <CardHeader>
+                                <CardTitle className="flex justify-between">
+                                    <div>Card Title</div>
+                                    <CircleDollarSign />
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>Card Content</p>
+                            </CardContent>
+                            <CardFooter>
+                                <p>Card Footer</p>
+                            </CardFooter>
+                        </Card>
+                    </div>
+                    <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
 }
