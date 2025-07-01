cffd52c8477566534c0484ea01652f0b9728ceb2
Hafza <muhammadhafzaaldian@gmail.com>
2025-07-01 15:52:33 +0700

    DashboardMain error debug

--- ubigcod/src/app/dashboard/component/header.tsx
+++ ubigcod/src/app/dashboard/component/header.tsx
@@ -55,7 +55,7 @@ export default function DashboardHeader() {
             <div className="flex items-center gap-2 ml-auto mr-11">
                 <Button variant="outline" size="icon" onClick={toggleTheme} className="relative">
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 duration-300" />
                 </Button>
             </div>
         </header>
